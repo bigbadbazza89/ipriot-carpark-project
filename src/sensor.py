09e2af90fb122185a58583927bd31c969726024e
Barrie Seldon <barrieseldon@gmail.com>
2025-06-10 15:18:49 +0800

    feat: add constructor, attributes and subclasses with sensor class

--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,11 @@
 class Sensor():
-    pass
+    def __init__(self, id, is_active, car_park):
+        self.id = id
+        self.is_active = is_active
+        self.car_park = car_park
+
+class EntrySensor(Sensor):
+    ...
+
+class ExitSensor(Sensor):
+    ...
