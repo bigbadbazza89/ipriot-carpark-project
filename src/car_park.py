8626173e2b36d5bd96030c1a20ebf03c3b168e1c
Barrie Seldon <barrieseldon@gmail.com>
2025-06-10 14:58:09 +0800

    feat: add attributes and constructors to car park class

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,12 @@
 class CarPark():
-    pass
+    def __init__(self, location, capacity, plates=None, displays=None):
+        self.location = location
+        self.displays = displays or []
+        self.capacity = capacity
+        self.plates = plates or []
+
+    def __str__(self):
+        return f"{self.location} Car Park - {self.capacity - len(self.plates)} bays available"
+
+cp = CarPark("Moondalup NMT", 100)
+print(cp)
