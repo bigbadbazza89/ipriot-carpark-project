a87c35883ea21a9127d0a916ec92b9063d63673c
Barrie Seldon <barrieseldon@gmail.com>
2025-06-10 15:12:23 +0800

    feat: add constructors and attributes to display class

--- src/display.py
+++ src/display.py
@@ -1,2 +1,8 @@
 class Display():
-    pass
+    def __init__(self, id, message, is_on):
+        self.id = id
+        self.message = "Welcome to the City of Moondalup!"
+        self.is_on = True
+
+    def __str__(self):
+        return f"{self.id}: {self.message}"
