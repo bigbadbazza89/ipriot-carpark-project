1f22fa3311a861e95526fc7f406b60d9c3373642
Barrie Seldon <barrieseldon@gmail.com>
2025-06-17 15:38:20 +0800

    feat: add update method for Display class.

--- src/display.py
+++ src/display.py
@@ -1,8 +1,20 @@
-class Display():
-    def __init__(self, id, message, is_on):
-        self.id = id
-        self.message = "Welcome to the City of Moondalup!"
-        self.is_on = True
+from time import sleep
+
+class Display:
+    def __init__(self, display_id, message="", is_on=False):
+        self.id = display_id
+        self.message = message
+        self.is_on = is_on
+
+    def update(self, data: dict, delay=0.2):
+        for key, value in data.items():
+            for c in f"{key}: {value}":
+                print(c, end="")
+                sleep(delay)
+            sleep(delay * 2)
+            print(end="\r")
+
+
 
     def __str__(self):
         return f"{self.id}: {self.message}"
