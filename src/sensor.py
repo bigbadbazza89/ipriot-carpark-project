8286b31b819df2abe598377860abcbc74e97174e
Barrie Seldon <barrieseldon@gmail.com>
2025-06-17 15:42:08 +0800

    feat: add @abstractmethod update_car_park, private method _scan_plate and detect_vehicle method.

--- src/sensor.py
+++ src/sensor.py
@@ -1,11 +1,40 @@
-class Sensor():
-    def __init__(self, id, is_active, car_park):
-        self.id = id
+from importlib.metadata import entry_points
+
+from car_park import CarPark
+from abc import ABC, abstractmethod
+import random
+
+class Sensor(ABC):
+    def __init__(self, sensor_id: str, car_park: CarPark, is_active: bool):
+        self.id = sensor_id
         self.is_active = is_active
         self.car_park = car_park
 
+    def _scan_plate(self):
+        return f"FAKE-{random.randint(0, 9999): 04d}"
+
+    def detect_vehicle(self):
+        plate = self._scan_plate()
+        self.update_car_park(plate)
+
+    @abstractmethod
+    def update_car_park(self, plate):
+        pass
+
+
+    def __str__(self):
+        return f"Sensor {self.id}: In {self.car_park}, Active: { self.is_active}"
+
+
 class EntrySensor(Sensor):
-    ...
+    def update_car_park(self, plate):
+        self.car_park.add_car(plate)
+        print(f"Inbound Vehicle Detected. Plate: {plate}")
 
 class ExitSensor(Sensor):
-    ...
+    def update_car_park(self, plate):
+        self.car_park.remove_car(plate)
+        print(f"Outbound Vehicle Detected. Plate: {plate}")
+
+    def _scan_plate(self):
+        return random.choice(self.car_park.plates)
