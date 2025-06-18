bc3d8bf8dcd7bbb7be5f542f0cce96e02c51ecf7
Barrie Seldon <barrieseldon@gmail.com>
2025-06-18 16:35:56 +0800

    fix: ignore import display from car_park.py and add default values for CarPark object created with Display setUp.

--- src/display.py
+++ src/display.py
@@ -1,20 +1,15 @@
-from time import sleep
-
+from car_park import CarPark
 class Display:
-    def __init__(self, display_id=int, message="Welcome and Thanks For Choosing Us!", is_on=True):
+    def __init__(self, display_id: int, message: str, car_park: CarPark, is_on=True):
         self.id = display_id
         self.message = message
         self.is_on = is_on
+        self.car_park = car_park
 
-    def update(self, data: dict, delay=0.2):
+    def update(self, data: dict):
         for key, value in data.items():
-            for c in f"{key}: {value}":
-                print(c, end="")
-                sleep(delay)
-            sleep(delay * 2)
-            print(end="\r")
-
-
+            if hasattr(self, key):
+                setattr(self, key, value)
 
     def __str__(self):
         return f"{self.id}: {self.message}"
