dc64d7e19965780703ff68ec9abf6eb9ceabf845
Barrie Seldon <barrieseldon@gmail.com>
2025-06-19 17:10:14 +0800

    feat: add @classmethod and write_config method for importing json configs and writing to a file in json.

--- src/car_park.py
+++ src/car_park.py
@@ -1,20 +1,29 @@
 from pathlib import Path
 from datetime import datetime
 from display import Display
+import json
 
 
 class CarPark:
-    def __init__(self, location, capacity, _plates=None, _displays=None, log_file=Path("log.txt")):
+    def __init__(self, location, capacity, _plates=None, _displays=None, log_file=Path("log.txt"), config_file=Path("config.json")):
         self.location = location
         self.displays = _displays or []
         self.capacity = capacity
         self.plates = _plates or []
         self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
+        self.config_file = Path(config_file) if not isinstance(config_file, Path) else config_file
         self.log_file.touch(exist_ok=True)
 
     @property
     def available_bays(self):
         return max(0, self.capacity - len(self.plates))
+
+    @classmethod
+    def from_config(cls, config_file=Path("config.json")):
+        config_file = config_file if isinstance(config_file, Path) else Path(config_file)
+        with config_file.open() as f:
+            config = json.load(f)
+        return cls(config["location"], config["capacity"], log_file=config["log_file"])
 
     def register(self, component):
         if not isinstance(component, Display):
@@ -44,7 +53,13 @@ class CarPark:
 
     def _log_car_activity(self, plate, action):
         with self.log_file.open("a") as f:
-            f.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
+            f.write(f"{plate} {action} at {datetime.now()}\n")
+
+    def write_config(self):
+        with self.config_file.open("w") as f:
+            json.dump({"location": self.location,
+                       "capacity": self.capacity,
+                       "log_file": str(self.log_file)}, f)
 
     def __str__(self):
         return f"{self.location} Car Park - {self.capacity - len(self.plates)} bays available"
